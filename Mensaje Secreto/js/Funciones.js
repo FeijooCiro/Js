91ec7a7ae6d4b48ba4e083718db48bf19cc80c41
FeijooCiro <128099617+FeijooCiro@users.noreply.github.com>
2023-03-31 19:13:10 -0300

    Add files via upload

---
+++ Mensaje Secreto/js/Funciones.js
@@ -0,0 +1,5 @@
+function SECRETO(cad){
+    return cad.split("").reverse().join(""); 
+    //Separa la cadena con split(""). La invierte por separado con reverse(). Y une todos los elementos de la cadena separada e invertida con join("")
+}
+ 
